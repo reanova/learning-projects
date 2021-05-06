a8d07b942daf0993f80782619a4d48a5116a3da3
Rea Nova <rea.novakovic@gmail.com>
2021-05-06 08:34:56 +0200

    Delete finalproject-drummachine directory

--- finalproject-drummachine/my-app/src/engines/engines.d.ts
+++
@@ -1,9 +0,0 @@
-export interface InstrumentEngine {
-    tone: number;
-    decay: number;
-    volume: number;
-    setup: () => void;
-    trigger: (time: number) => void;
-    setTone: (tone: number) => void;
-    setVolume: (tone: number) => void;
-}
